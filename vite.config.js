6c85286b1a696407af9fb7a4543e25e0375a04e9
danil_sitdikov <quadrad4@gmail.com>
2025-09-01 15:46:46 +0600

    fix 404

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  base: '/lang-app/',
+  base: '/lang-app',
   build: {
     outDir: 'dist',
   },
