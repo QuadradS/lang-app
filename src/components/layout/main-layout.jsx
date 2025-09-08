b36fabadd06af946fc5ce0db4c18385b78b6ae11
danil_sitdikov <quadrad4@gmail.com>
2025-09-08 15:15:18 +0600

    new header

--- src/components/layout/main-layout.jsx
+++ src/components/layout/main-layout.jsx
@@ -1,4 +1,6 @@
 import { Menubar } from 'primereact/menubar';
+import {Link} from "react-router";
+import {Button} from "primereact/button";
 
 export const MainLayout = ({children}) => {
   const items = [
@@ -16,8 +18,18 @@ export const MainLayout = ({children}) => {
   ];
   return (
     <main className="relative">
-      <header className="w-full max-w-[1280px] mx-auto ">
-        <Menubar model={items} />
+      <header className="w-full max-w-[1280px] mx-auto px-3">
+        <Link className="text-md" to="/">
+          <Button severity="info" text>Manage words</Button>
+        </Link>
+        <Link className="text-md ml-4" to="/learn">
+          <Button severity="info" text>Learn</Button>
+        </Link>
+        <Link className="text-md ml-4" to="/memory-texts">
+          <Button severity="info" text>
+            Memory texts
+          </Button>
+        </Link>
       </header>
       <div className="px-3 py-2 w-full min-h-[100vh] max-w-[1280px] mx-auto">
         {children}
